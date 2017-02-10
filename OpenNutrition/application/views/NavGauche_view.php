d11b685749eea954aebdceb2a5773e35703afd1e
petitabdel <petitabdel@live.fr>
2017-02-10 21:52:45 +0100

    Ajout du lien vers le nouveau controleur Connexion

--- OpenNutrition/application/views/NavGauche_view.php
+++ OpenNutrition/application/views/NavGauche_view.php
@@ -2,6 +2,7 @@
 			<span class="mdl-layout-title">Accueil</span>
 			    <nav class="mdl-navigation">
 			      <a class="mdl-navigation__link" href="<?php echo base_url('index.php/Welcome');?>">Accueil</a>
+			      <a class="mdl-navigation__link" href="<?php echo base_url('index.php/Connexion');?>">Connexion</a>
 			      <a class="mdl-navigation__link" href="<?php echo base_url('index.php/Formulaire');?>">Gerer les plats</a>
 			    </nav>
 		  </div>
