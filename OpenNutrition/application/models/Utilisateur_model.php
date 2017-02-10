127a62e133651756dd2bf2ec5f7f415f79cefdf9
petitabdel <petitabdel@live.fr>
2017-02-10 22:54:41 +0100

    Ajout du model utilisateur_model

---
+++ OpenNutrition/application/models/Utilisateur_model.php
@@ -0,0 +1,18 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Utilisateur_model
+ *
+ * @author Abdel
+ */
+class Utilisateur_model extends CI_Model   {
+    //put your code here
+}
