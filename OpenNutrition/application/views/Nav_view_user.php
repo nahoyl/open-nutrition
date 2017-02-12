ceabeddd6a8bf664a5d4ab671d2abb48139922f1
petitabdel <petitabdel@live.fr>
2017-02-12 01:55:07 +0100

    Ajout du Nav view pour les utilisateurs

---
+++ OpenNutrition/application/views/Nav_view_user.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header
+     mdl-layout--fixed-tabs">
+    <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+            <span class="mdl-layout-title">Open Nutrition</span>
+        </div>
+        <div class="mdl-layout__tab-bar mdl-js-ripple-effect">
+            <a href="#fixed-tab-ajout" class="mdl-layout__tab is-active">Ajouter</a>
+            <a href="#fixed-tab-suppr" class="mdl-layout__tab">Supprimer</a>
+        </div>
+    </header>
