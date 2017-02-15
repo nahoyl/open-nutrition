245fcb2b782a6b154a2878f219444e1939ef35c6
petitabdel <petitabdel@live.fr>
2017-02-15 21:38:11 +0100

    Changement de place  <script>

--- OpenNutrition/application/views/Footer_view.php
+++ OpenNutrition/application/views/Footer_view.php
@@ -1,7 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-		</div>
-                
-	</body>
+</div>
+
+<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+<script defer src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
+<!--<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.js"></script>-->
+<script defer src="<?php echo js_url('gestionPanier'); ?>"></script>
+<script defer src="<?php echo js_url('dialog-polyfill'); ?>"></script>
+<script defer src="<?php echo js_url('dialogAllergenes'); ?>"></script>
+<script defer src="<?php echo js_url('suggestion'); ?>"></script>
+
+</body>
 </html>
